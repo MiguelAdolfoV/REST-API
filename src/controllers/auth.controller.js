5c0d0b87f2a8b7f2ec927af9dd5b85047e22ac82
Ninis <MiguelAdolfoV@outlook.com>
2025-03-28 00:45:56 -0600

    Añadir verificación de correo electrónico en el registro de usuario

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,35 +8,42 @@ import * as token from "../controllers/token.controller.js"
 //importar mensajes
  import * as messages from "../../Art/Messages.js"
 
-export const signUp = async (req,res) => {
-    try{
-         //extraer datos de peticion
-    const {username,email,password} = req.body;
-    //crear nuevo usuario
-    const newUser = new User({
-        username,
-        email,
-        password: await User.encryptPassword(password)
-    });
+ export const signUp = async (req, res) => {
+    try {
+        // Extraer datos de la petición
+        const { username, email, password } = req.body;
 
-    if(req.body.roles){
-        const foundRoles = await Role.find({ name: {$in: req.body.roles} });
-        newUser.roles = foundRoles.map(role => role.id);
-    } else {
-        const role = await Role.findOne({ name: "customer"});
-        newUser.roles = [role.id];
-    }
+        // Verificar si el correo ya está registrado
+        const emailExists = await User.findOne({ email });
+        if (emailExists) {
+            return res.status(400).json({ message: "El correo ya está registrado" });
+        }
 
-    const saveUser = await newUser.save();
-    console.log(saveUser);
+        // Crear nuevo usuario
+        const newUser = new User({
+            username,
+            email,
+            password: await User.encryptPassword(password),
+        });
 
-    //responder
-    res.json(messages.savedUserSimple)
+        if (req.body.roles) {
+            const foundRoles = await Role.find({ name: { $in: req.body.roles } });
+            newUser.roles = foundRoles.map(role => role.id);
+        } else {
+            const role = await Role.findOne({ name: "customer" });
+            newUser.roles = [role.id];
+        }
+
+        const saveUser = await newUser.save();
+        console.log(saveUser);
+
+        // Responder
+        res.json(messages.savedUserSimple);
     } catch (error) {
-        console.error(error);
-
-    }   
-}
+        console.error("Error in signUp:", error);
+        res.status(500).json({ message: "Ha ocurrido un error en el servidor" });
+    }
+};
 
 export const signIn = async (req, res) => {
     try {
