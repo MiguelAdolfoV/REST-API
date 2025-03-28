999cb55873a9d2437271775f5900aa8f1186c6dc
Ninis <MiguelAdolfoV@outlook.com>
2025-03-28 10:01:58 -0600

    Refactor ingreso routes to add meta management endpoints and improve route organization

--- src/routes/ingreso.routes.js
+++ src/routes/ingreso.routes.js
@@ -8,12 +8,12 @@ import { authJwt } from "../middlewares/index.js";
 
 // Establecer rutas para ingreso
 router.get('/', [authJwt.verifyToken, authJwt.isCustomer], ingresoCtrl.getIngreso);
-router.get('/:ingresoId', [authJwt.verifyToken, authJwt.isCustomer], ingresoCtrl.getIngresoById);
-router.post('/', [authJwt.verifyToken, authJwt.isCustomer], ingresoCtrl.createIngreso);
-router.put('/:ingresoId', [authJwt.verifyToken, authJwt.isAdmin], ingresoCtrl.updateIngreso);
-router.delete('/:ingresoId', [authJwt.verifyToken, authJwt.isAdmin], ingresoCtrl.deleteIngreso);
-router.post('/consejo', [authJwt.verifyToken, authJwt.isCustomer], ingresoCtrl.getConsejo);
 router.post('/metas', [authJwt.verifyToken, authJwt.isCustomer], ingresoCtrl.createMeta);
 router.get('/metas', [authJwt.verifyToken, authJwt.isCustomer], ingresoCtrl.getMetas);
+router.post('/', [authJwt.verifyToken, authJwt.isCustomer], ingresoCtrl.createIngreso);
+router.post('/consejo', [authJwt.verifyToken, authJwt.isCustomer], ingresoCtrl.getConsejo);
+router.get('/:ingresoId', [authJwt.verifyToken, authJwt.isCustomer], ingresoCtrl.getIngresoById);
+router.put('/:ingresoId', [authJwt.verifyToken, authJwt.isAdmin], ingresoCtrl.updateIngreso);
+router.delete('/:ingresoId', [authJwt.verifyToken, authJwt.isAdmin], ingresoCtrl.deleteIngreso);
 
 export default router;
